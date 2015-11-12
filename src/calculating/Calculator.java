523ea184a7412df2fe047af8f80014538db9e61c
Martyna <anielska.martyna@gmail.com>
2015-11-12 12:55:14 +0100

    Refaktoryzacja kodu (Calculator.java) Dodanie nowych testów

--- src/calculating/Calculator.java
+++ src/calculating/Calculator.java
@@ -9,73 +9,126 @@ import java.util.ArrayList;
  */
 public final class Calculator {
 
-    public Calculator()
-    {
+    public Calculator(){
 
     }
 
     public static float evaluate(String expression)
     {
+        float result=0F;
+
         List<Float> numbers = new ArrayList<Float>();
         List<Character> operators = new ArrayList<Character>();
 
 
-        String tmp[] = expression.split("[+\\-*/]");
+        numbers = splitNumbers(expression);
+        operators = splitOperators(expression);
 
+       //System.out.println(numbers);
+       //System.out.println(operators);
 
-        for(String i: tmp) {
-            i.trim();
-            numbers.add(Float.parseFloat(i));
-        }
-
-        tmp = expression.split("[0-9.\\t\\n\\x0b\\r\\f]+");
-
-
-        for(int i=1; i<tmp.length; i++) {
-            operators.add(tmp[i].charAt(0));
-        }
-
-
-//       System.out.println(numbers);
-//       System.out.println(operators);
-//       System.out.println(numbers.get(0));
-//       System.out.println(numbers.get(1));
-
-        Float result=0F;
 
         switch (operators.get(0)) {
 
             case'+':
             {
-                result = numbers.get(0)+numbers.get(1);
+                result = add(numbers.get(0), numbers.get(1));
                 break;
             }
             case'-':
             {
-                result = numbers.get(0)-numbers.get(1);
+                result = subtract(numbers.get(0), numbers.get(1));
                 break;
             }
             case'*':
             {
-                result = numbers.get(0)*numbers.get(1);
+                result = multiply(numbers.get(0), numbers.get(1));
                 break;
             }
             case'/':
             {
-                if(numbers.get(1)!=0)
-                    result = numbers.get(0)/numbers.get(1);
-                else
-                    throw new IllegalArgumentException("Nie mozna dzielic przez 0!");
+                result = divide(numbers.get(0), numbers.get(1));
                 break;
             }
             default:
             {
-
                 result = 0F;
                 break;
             }
         }
 
         return result;
+    }
+
+
+    private static boolean isFirstNegative(String expression){
+
+        if(expression.charAt(0)=='-')
+            return true;
+        else
+            return false;
+    }
+
+    private static float add(float a, float b){
+
+        float result = a + b;
+        return result;
+    }
+
+    private static float subtract(float a, float b){
+
+        float result = a - b;
+        return result;
+    }
+
+    private static float multiply(float a, float b){
+
+        float result = a * b;
+        return result;
+    }
+
+    private static float divide(float a, float b){
+
+
+        float result = a/b;
+        return result;
+    }
+
+    private static ArrayList<Float> splitNumbers(String expression){
+
+        ArrayList<Float> numbers = new ArrayList<Float>();
+
+        String splitedExpression[] = expression.split("[+\\-*/]");
+
+        if(!isFirstNegative(expression)) {
+            for (String i : splitedExpression) {
+                i.trim();
+                numbers.add(Float.parseFloat(i));
+            }
+        }
+        else{
+            numbers.add(-Float.parseFloat(splitedExpression[1]));
+            for(int i=2; i<splitedExpression.length; i++) {
+                splitedExpression[i].trim();
+                numbers.add(Float.parseFloat(splitedExpression[i]));
+            }
+        }
+
+        return numbers;
+    }
+
+    private static ArrayList<Character> splitOperators(String expression){
+
+        ArrayList<Character> operators = new ArrayList<Character>();
+
+        String splitedExpression[] = expression.split("[0-9.\\w]+");
+
+
+        for(int i=1; i<splitedExpression.length; i++) {
+            operators.add(splitedExpression[i].charAt(0));
+        }
+
+        return operators;
+
     }
 }
--- tests/calculating/CalculatorTest.java
+++ tests/calculating/CalculatorTest.java
@@ -7,12 +7,21 @@ import static org.junit.Assert.*;
 /**
  * Created by Martyna on 2015-10-29.
  */
+
+/*
+** Integers: Two Positive
+** Integers: First Negative
+** Integers: Second Negative(loading...)
+** Integers: Two Negative(loading...)
+** Reals: Two Positive
+ */
 public class CalculatorTest {
 
     @Test
     public void testTrueShouldBeEqualTrue() {
         assertEquals("true equals true", true, true == true);
     }
+/* Positive Integers */
 
     @Test
     public void evaluate_additionOfTwoPositiveIntegers_ok() {
@@ -25,12 +34,32 @@ public class CalculatorTest {
     }
 
     @Test
-    public void evaluate_subtractionOfTwoPositiveIntegers_ok() {
+    public void evaluate_subtractionOfTwoPositiveIntegersPositiveResult_ok() {
 
         assertEquals(3F, Calculator.evaluate("5-2"), 0.01);
-        assertEquals(-3F, Calculator.evaluate("2-5"), 0.01);
         assertEquals(8F, Calculator.evaluate("10-2"), 0.01);
         assertEquals(80F, Calculator.evaluate("100-20"), 0.01);
+        assertEquals(5F, Calculator.evaluate("5-0"), 0.01);
+    }
+
+    @Test
+    public void evaluate_subtractionOfTwoPositiveIntegersNegativeResult_ok() {
+
+        assertEquals(-3F, Calculator.evaluate("2-5"), 0.01);
+        assertEquals(-8F, Calculator.evaluate("2-10"), 0.01);
+        assertEquals(-80F, Calculator.evaluate("20-100"), 0.01);
+        assertEquals(-5F, Calculator.evaluate("0-5"), 0.01);
+    }
+
+    @Test
+    public void multiplyingTwoPositiveIntegers() {
+
+        assertEquals(10F, Calculator.evaluate("5*2"), 0.01);
+        assertEquals(100F, Calculator.evaluate("50*2"), 0.01);
+        assertEquals(100F, Calculator.evaluate("10*10"), 0.01);
+        assertEquals(5F, Calculator.evaluate("5*1"), 0.01);
+        assertEquals(0F, Calculator.evaluate("5*0"), 0.01);
+
     }
 
     @Test
@@ -43,19 +72,62 @@ public class CalculatorTest {
         assertEquals(0.01F, Calculator.evaluate("1/100"), 0.01);
     }
 
-   @Test (expected = ArithmeticException.class)
-    public void evaluate_dividingByZero_ex() throws Exception {
-        Calculator.evaluate("2/0");
+   @Test
+   public void evaluate_dividingByZeroFirstPositive_ok() {
 
+       assertEquals(Float.POSITIVE_INFINITY, Calculator.evaluate("2/0"), 0.01);
+
+   }
+
+
+/* Negative-First Integers */
+
+
+    @Test
+    public void evaluate_additionOfTwoIntegersFirstNegative_ok() {
+
+        assertEquals( 0F, Calculator.evaluate("-2+2"), 0.01);
+        assertEquals( -2F, Calculator.evaluate("-2+0"), 0.01);
+        assertEquals( -8F, Calculator.evaluate("-10+2"), 0.01);
+        assertEquals( -98F, Calculator.evaluate("-100+2"), 0.01);
     }
 
     @Test
-    public void multiplyingTwoPositiveIntegers() {
+    public void evaluate_subtractionOfTwoIntegersFirstNegative_ok() {
 
-        assertEquals(10F, Calculator.evaluate("5*2"), 0.01);
-        assertEquals(100F, Calculator.evaluate("50*2"), 0.01);
-        assertEquals(100F, Calculator.evaluate("10*10"), 0.01);
+        assertEquals(-7F, Calculator.evaluate("-5-2"), 0.01);
+        assertEquals(-7F, Calculator.evaluate("-2-5"), 0.01);
+        assertEquals(-12F, Calculator.evaluate("-10-2"), 0.01);
+        assertEquals(-120F, Calculator.evaluate("-100-20"), 0.01);
     }
+
+    @Test
+    public void multiplyingTwoIntegersFirstNegative() {
+
+        assertEquals(-10F, Calculator.evaluate("-5*2"), 0.01);
+        assertEquals(-100F, Calculator.evaluate("-50*2"), 0.01);
+        assertEquals(-100F, Calculator.evaluate("-10*10"), 0.01);
+    }
+
+    @Test
+    public void evaluate_dividingTwoIntegersFirstNegative_ok() {
+
+        assertEquals(-3F, Calculator.evaluate("-9/3"), 0.01);
+        assertEquals(-3.33F, Calculator.evaluate("-10/3"), 0.01);
+        assertEquals(-2.5F, Calculator.evaluate("-5/2"), 0.01);
+        assertEquals(-0.5F, Calculator.evaluate("-1/2"), 0.01);
+        assertEquals(-0.01F, Calculator.evaluate("-1/100"), 0.01);
+    }
+
+    @Test
+    public void evaluate_dividingByZeroFirstNegative_ok() {
+
+        assertEquals(Float.NEGATIVE_INFINITY, Calculator.evaluate("-2/0"), 0.01);
+
+    }
+
+
+ /* Positive Reals */
 
     @Test
     public void evaluate_additionOfTwoPositiveReals_ok() {
@@ -84,4 +156,8 @@ public class CalculatorTest {
         assertEquals(0.01F, Calculator.evaluate("1-0.99"), 0.01);
 
     }
+
+/* Negative Reals */
+
+
 }
